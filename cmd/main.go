e826725d6961c4e925eaca3e1c6925222c6b27d3
Rajesh Bhusal <tipstricks118@gmail.com>
2025-08-25 09:03:25 +0545

    Added the basic flag support

---
+++ cmd/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	source      string
+	destination string
+	recursive   bool
+	verbose     bool
+)
+
+// Th go-sync is the main entry point of the application
+// Usage: go-sync flags
+var rootCmd = &cobra.Command{
+	Use:   "go-sync",
+	Short: "A fast, lightweight file synchronization tool written in Go.",
+	Long: `Go Sync is designed to efficiently synchronize files and directories 
+between local and remote systems. It uses intelligent algorithms to detect 
+changes and transfer only the necessary data, minimizing bandwidth and 
+speeding up sync operations.`,
+	Run: runSync,
+}
+
+// The sync command handles the actual synchronization process
+// Usage: go-sync sync flags
+var syncCmd = &cobra.Command{
+	Use:   "sync",
+	Short: "Synchronize files and directories",
+	Long:  `Synchronize files and directories from source to destination`,
+	Run:   runSync,
+}
+
+func init() {
+	// Go automatically calls the init function before the main function
+	addFlags(rootCmd)
+	addFlags(syncCmd)
+
+	rootCmd.AddCommand(syncCmd)
+}
+
+func runSync(cmd *cobra.Command, args []string) {
+	if _, err := os.Stat(source); os.IsNotExist(err) {
+		println("Source path does not exist:", source)
+		return
+	}
+	if verbose {
+		fmt.Printf("Starting synchronization from %s to %s", source, destination)
+		fmt.Printf("Recursive: %v", recursive)
+		fmt.Printf("Verbose: %v", verbose)
+		fmt.Printf("Starting sync...")
+	} else {
+		fmt.Printf("Syncing '%s' to '%s'", source, destination)
+	}
+
+	// TODO:- Call the actual sync function from the sync package
+
+}
+
+func addFlags(cmd *cobra.Command) {
+	cmd.Flags().StringVarP(&source, "source", "s", "", "Source path (local or remote)")
+	cmd.Flags().StringVarP(&destination, "destination", "d", "", "Destination path (local or remote)")
+	cmd.Flags().BoolVarP(&recursive, "recursive", "r", false, "Recursively sync directories")
+	cmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose output")
+
+	cmd.MarkFlagRequired("source")
+	cmd.MarkFlagRequired("destination")
+}
+
+func main() {
+	fmt.Println("Starting application...")
+	if err := rootCmd.Execute(); err != nil {
+		println("Error executing command:", err.Error())
+		os.Exit(1)
+	}
+}
